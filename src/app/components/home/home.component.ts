e95526bf2b9e23fa039637ee6ee2a0d29b79cfb6
APG <apg.prywatny@gmail.com>
2021-07-13 20:50:21 +0200

    added http service, interceptors, assets

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {APIResponse, Game} from "../../interface-models";
+import {HttpService} from "../../services/http.service";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public sort: string;
+  public games: Array<Game>;
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search'])
+      } else {
+        this.searchGames('metacrit')
+      }
+    })
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService.getGameList(sort,search).subscribe((gameList: APIResponse<Game>) => {
+      this.games = gameList.results
+      console.log(gameList)
+    })
   }
 
 }
---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,23 @@
+import {Injectable} from "@angular/core";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+
+      setParams: {
+        key: 'c44d7c9ea1814a10a628bdcc8afef385'
+      }
+
+    })
+    return next.handle(req);
+  }
+}
