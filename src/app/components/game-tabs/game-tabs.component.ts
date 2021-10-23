12a5195858119f9cdcca2edee5aaf2c71b97e6bd
APG <apg.prywatny@gmail.com>
2021-10-23 21:02:28 +0200

    add game details tabs

---
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Game} from "../../interface-models";
+
+@Component({
+  selector: 'app-game-tabs',
+  templateUrl: './game-tabs.component.html',
+  styleUrls: ['./game-tabs.component.scss']
+})
+export class GameTabsComponent implements OnInit, OnDestroy {
+  @Input() game: Game
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+  }
+
+
+}
